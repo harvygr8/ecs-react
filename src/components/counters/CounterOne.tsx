565ea82182746fc7d4b1d20ff215e7b6c7ac3b3a
Harvinder Singh <60150871+harvygr8@users.noreply.github.com>
2024-07-10 22:41:09 -0500

    ExpOne

---
+++ src/components/counters/CounterOne.tsx
@@ -0,0 +1,26 @@
+import { Box, Button, AbsoluteCenter, Flex, Text } from '@chakra-ui/react'
+import React, { useState } from 'react'
+
+
+//Basic State
+const CounterOne = () => { 
+
+  const [count,setCount] = useState(0)
+
+  return (
+    <Box bg={'teal.700'} borderRadius={'6px'} p={2} color={'white'} h='120px' w='150px'>
+        <AbsoluteCenter>
+            <Flex direction={'column'} alignItems={'center'} gap={2}>
+                <Text size={'xl'} fontWeight={'bold'}>CounterOne</Text>
+                <Text size={'4xl'}>{count}</Text>
+                <Button size={'xs'} onClick={()=>setCount(count+1)}>
+                    Increment
+                </Button>
+            </Flex>
+        </AbsoluteCenter>
+    </Box>
+
+  )
+}
+
+export default CounterOne
---
+++ src/experiments/ExpOne.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import CounterOne from '../components/counters/CounterOne'
+import { Center } from '@chakra-ui/react'
+
+const ExpOne = () => {
+  return (
+    <Center height={'100vh'}>
+        <CounterOne/>
+    </Center>
+  )
+}
+
+export default ExpOne
